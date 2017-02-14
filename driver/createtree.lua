861353099571ba8bf80c28f154ce5da8aa682104
Eric Moreira <Eric Moreira>
2017-02-14 02:00:17 -0200

    Added clipping test for linear segments

--- driver/createtree.lua
+++ driver/createtree.lua
@@ -1,15 +1,147 @@
 local table = require"table"
 
-local function Inside(x, y, xmax, xmin, ymax, ymin)
+local function inside(x, y, xmin, ymin, xmax, ymax)
 if y <= ymax and y >= ymin and x <= xmax  and x >= xmin then
 		return true
 else
 		return false
 end
 
-local function ShapeInsideScene(scene,bb,element_id)
-  local shape = scene.shapes[element_id]
+function vertical_test_linear_segment(x1, y1, x2, y2, x, y)
+  local valor = (y2 - y1)*x + (x1 - x2)*y - x1*(y2 - y1) - y1*(x1 - x2)
+  return (x2 - x1)*valor < 0 
+end
+
+function horizontal_test_linear_segment(x1, y1, x2, y2, x, y)
+  local valor = (y2 - y1)*x + (x1 - x2)*y - x1*(y2 - y1) - y1*(x1 - x2)
+  return (x2 - x1)*valor < 0 
+end
+
+function vertical_linear_test(x0,y0,x1,y1,x,y,xmin,ymin,xmax,ymax)
+  if xmin <= x and x <= xmax and y >= ymin then 
+    if y > ymax then return true
+    else return test_linear_segment(x0, y0, x1, y1, x, y) end
+  end
+
+  return false
+end
+
+function horizontal_linear_test(x0,y0,x1,y1,x,y,xmin,ymin,xmax,ymax)
+  if ymin <= y and y <= ymax and x <= xmax then
+    if x < xmin then return true
+    else return test_linear_segment(x0, y0, x1, y1, x, y) end
+  end
+
+  return false
+end
+
+function intersects_linear_segment(xmin,ymin,xmax,ymax,x0,y0,x1,y1)
+  if vertical_linear_test(x0,y0,xclose,yclose,xmax,ymax,xmin,ymin,xmax,ymax) == true and vertical_linear_test(x0,y0,xclose,yclose,xmax,ymin,xmin,ymin,xmax,ymax) == false then
+    return true
+  elseif vertical_linear_test(x0,y0,xclose,yclose,xmin,ymax,xmin,ymin,xmax,ymax) == true and vertical_linear_test(x0,y0,xclose,yclose,xmin,ymin,xmin,ymin,xmax,ymax) == false then
+    return true
+  elseif horizontal_linear_test(x0,y0,xclose,yclose,xmin,ymin,xmin,ymin,xmax,ymax) == true and horizontal_linear_test(x0,y0,xclose,yclose,xmin,ymax,xmin,ymin,xmax,ymax) == false then
+    return true
+  elseif horizontal_linear_test(x0,y0,xclose,yclose,xmax,ymin,xmin,ymin,xmax,ymax) == true and horizontal_linear_test(x0,y0,xclose,yclose,xmax,ymax,xmin,ymin,xmax,ymax) == false then
+    return true
+  else
+    return false
+end
+
+local function ShapeInsideScene(scene,bb,shape)
   local xmax, xmin ,ymax, ymin = unpack(shape.supdata)
+  -- Converte não-paths para paths
+  if shape.type ~= "path" then shape = shape:as_path(shape, shape.xf) end
+
+  local new_path = path.path()
+
+  --Por enquanto vou percorrer na mão mesmo por aqui. Se quiser um path.iterate(), a gente discute depois
+  --Também não vou implementar sua ideia dos bcc e occ
+  local begin = false
+  local dat = 1
+  local xclose, yclose
+
+  for i=1,#path.instructions do
+    local instruction = path.instructions[i]
+
+    if instruction == "begin_closed_contour" or instruction == "begin_open_contour" then
+      begin = true
+      dat = dat + 1
+    end
+            
+    if instruction == "end_open_contour" or instruction == "end_closed_contour" then
+      local x0,y0 = unpack(path.data,dat,dat+1)
+      local isInside = false
+
+      --Primeiro, testo os endpoints
+      if inside(x0,y0,xmin,ymin,xmax,ymax) == true or inside(xclose,yclose,xmin,ymin,xmax,ymax) == true then isInside = true
+      --Agora testa as intersecções. Perceba que testo todas as arestas.
+      elseif intersects_linear_segment(xmin,ymin,xmax,ymax,x0,y0,xclose,yclose) == true then
+          isInside = true
+      end
+
+      --
+      if isInside == true then
+        path.push_data(new_path,x0,y0)
+        --Não entendi esse Rewind (que tem na função push_instruction em path.lua). Depois me avisa
+        path.push_instruction(new_path, instruction)
+      end
+
+      cont = cont + 1
+      dat = dat + 3
+    end
+
+    if instruction == "linear_segment" then
+      local x0,y0,x1,y1 = unpack(path.data,dat,dat+3)
+      if begin == true then
+        xclose, yclose = x0, y0
+        begin = false
+      end
+
+      wind_num = wind_num + path.winding[cont](x,y,element.winding_rule,cont)
+      cont = cont + 1
+      dat = dat + 2
+    end
+
+    if instruction == "cubic_segment" then
+      local x0,y0,x1,y1,x2,y2,x3,y3 = unpack(path.data,dat,dat+7)
+      if begin == true then
+        xclose, yclose = x0, y0
+        begin = false
+      end
+      wind_num = wind_num + path.winding[cont](x,y,element.winding_rule,cont,x0,y0,x1,y1,x2,y2,x3,y3)
+      cont = cont + 1
+      dat = dat + 6
+    end
+      
+    if instruction == "quadratic_segment" then
+      local x0,y0,x1,y1,x2,y2 = unpack(path.data,dat,dat+5)
+      if begin == true then
+        xclose, yclose = x0, y0
+        begin = false
+      end
+      wind_num = wind_num + path.winding[cont](x,y,element.winding_rule,cont)
+      cont = cont + 1
+      dat = dat + 4   
+    end
+
+    if instruction == "rational_quadratic_segment" then
+      local x0,y0,x1,y1,w1,x2,y2 = unpack(path.data,dat,dat+6)
+      if begin == true then
+        xclose, yclose = x0, y0
+        begin = false
+      end
+      wind_num = wind_num + path.winding[cont](x,y,element.winding_rule,cont)
+      cont = cont + 1
+      dat = dat + 5
+    end
+  end
+
+  return wind_num
+end
+
+
+  --[[
   -- gambiarra que tou fazendo aqui pra testar se o shape tá dentro da cena
   local x1, y1 = xmin, ymin
   local x2, y2 = xmax, ymin
@@ -25,19 +157,22 @@ local function ShapeInsideScene(scene,bb,element_id)
   elseif Inside(x4, y4, xmax, xmin, ymax, ymin) == true then
     return true
   end
+  ]]--
+
+
   -- outra gambiarra aqui, a cena pode ser contida totalmente pelo shape
   local bbx1, bby1 = xmin, ymin
   local bbx2, bby2 = xmax, ymin
   local bbx3, bby3 = xmin, ymax
   local bbx4, bby4 = xmax, ymax
   xmax, xmin ,ymax, ymin = unpack(shape.supdata)
-  if Inside(bbx1, bby1, xmax, xmin, ymax, ymin) == true then
+  if inside(bbx1, bby1, xmin, ymin, xmax, ymax) == true then
     return true
-  elseif Inside(bbx2, bby2, xmax, xmin, ymax, ymin) == true then
+  elseif inside(bbx2, bby2, xmin, ymin, xmax, ymax) == true then
     return true
-  elseif Inside(bbx3, bby3, xmax, xmin, ymax, ymin) == true then
+  elseif inside(bbx3, bby3, xmin, ymin, xmax, ymax) == true then
     return true
-  elseif Inside(bbx4, bby4, xmax, xmin, ymax, ymin) == true then
+  elseif inside(bbx4, bby4, xmin, ymin, xmax, ymax) == true then
     return true
   end
   return false
@@ -76,7 +211,7 @@ branch.bb = {xmax = xmax, xmin = xmin, ymax = ymax, ymin = ymin}
 branch.data = { }
 branch.numseg = 0
 for k,el in pairs(tree[camada].data) do
-  if ShapeInsideScene(scene, branch.bb, k) == true then
+  if ShapeInsideScene(scene, branch.bb, scene.shapes[k]) == true then
     -- retorna uma table com os seg que pertencem (a implementar)
     branch.data[k] = SegmentInsideScene()
     branch.numseg = branch.numseg + table.getn(branch.data[k])
